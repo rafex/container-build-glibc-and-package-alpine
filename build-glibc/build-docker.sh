b28f1fc19d71befe61d806a715122d1356849026
rafex <rafex@rafex.dev>
2024-03-21 22:00:50 -0600

    banderas para visualizar puntos

--- build-glibc/build-docker.sh
+++ build-glibc/build-docker.sh
@@ -6,7 +6,13 @@ GLIBC_VERSION=2.39
 
 # Build the image
 docker build --progress=plain -t rafex/glibc-builder .
+echo "Built rafex/glibc-builder"
 
 docker run --name glibc-binary rafex/glibc-builder ${GLIBC_VERSION} /usr/glibc-compat
+echo "Running rafex/glibc-builder"
+
 docker cp glibc-binary:/glibc-bin-${GLIBC_VERSION}.tar.gz ./bin/
+echo "Copied glibc-bin-${GLIBC_VERSION}.tar.gz"
+
 docker rm glibc-binary
+echo "Removed glibc-binary"
